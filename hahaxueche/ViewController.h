ddf34d3787b049e15c086fac3c891726ffee057d
Zixiao Wang <zixiao@tilt.com>
2015-07-04 09:34:52 -0500

    Initial Commit

---
+++ hahaxueche/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hahaxueche
+//
+//  Created by Zixiao Wang on 7/4/15.
+//  Copyright (c) 2015 Zixiao Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hahaxueche/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hahaxueche
+//
+//  Created by Zixiao Wang on 7/4/15.
+//  Copyright (c) 2015 Zixiao Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
