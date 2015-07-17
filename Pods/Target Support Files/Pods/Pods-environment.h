20f59e3d2ba11ed03014b80b3637599fd6af2835
Zixiao Wang <zixiao@tilt.com>
2015-07-17 09:31:41 -0500

    add pod

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -13,6 +13,12 @@
 // Please contact the author.
 // Version: 3.1.2.6.
 
+// JSONModel
+#define COCOAPODS_POD_AVAILABLE_JSONModel
+#define COCOAPODS_VERSION_MAJOR_JSONModel 1
+#define COCOAPODS_VERSION_MINOR_JSONModel 1
+#define COCOAPODS_VERSION_PATCH_JSONModel 0
+
 // JTSImageViewController
 #define COCOAPODS_POD_AVAILABLE_JTSImageViewController
 #define COCOAPODS_VERSION_MAJOR_JTSImageViewController 1
@@ -24,18 +30,6 @@
 #define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
 #define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
 #define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
-
-// Mantle
-#define COCOAPODS_POD_AVAILABLE_Mantle
-#define COCOAPODS_VERSION_MAJOR_Mantle 2
-#define COCOAPODS_VERSION_MINOR_Mantle 0
-#define COCOAPODS_VERSION_PATCH_Mantle 2
-
-// Mantle/extobjc
-#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
-#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 2
-#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 0
-#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 2
 
 // SDCycleScrollView
 #define COCOAPODS_POD_AVAILABLE_SDCycleScrollView
