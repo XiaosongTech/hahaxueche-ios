e20be821e7fb94437293b58b6747f554fcec401d
Zixiao Wang <zixiao@tilt.com>
2015-09-17 22:39:26 -0500

    build and height

--- hahaxueche/View Controllers/Root/HHMyProfileViewController.m
+++ hahaxueche/View Controllers/Root/HHMyProfileViewController.m
@@ -68,7 +68,7 @@
     self.settings = [UIBarButtonItem buttonItemWithTitle:NSLocalizedString(@"设置", nil) action:@selector(settingsTapped) target:self isLeft:NO];
     self.navigationItem.rightBarButtonItem = self.settings;
     
-    self.explanationLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds)-20.0f, 60.0f)];
+    self.explanationLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds)-20.0f, 70.0f)];
     self.explanationLabel.backgroundColor = [UIColor clearColor];
     self.explanationLabel.text = NSLocalizedString(@"注：学员支付的学费将由平台保管，每个阶段结束后，学员可以根据情况，点此阶段的付款按钮，点击后，平台将该阶段对应的金额转给教练，然后进入下一阶段。每个阶段到金额会在点击付款后的第一个周二转到教练账户。", nil);
     self.explanationLabel.textColor = [UIColor HHGrayTextColor];
