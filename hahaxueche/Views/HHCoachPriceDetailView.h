d95f21a4300daeb82886fbc7ef05cd3f1039c629
Zixiao Wang <zixiao@tilt.com>
2015-11-24 20:38:33 -0600

    no message

---
+++ hahaxueche/Views/HHCoachPriceDetailView.h
@@ -0,0 +1,15 @@
+//
+//  HHCoachPriceDetailView.h
+//  hahaxueche
+//
+//  Created by Zixiao Wang on 11/24/15.
+//  Copyright © 2015 Zixiao Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHCoachPriceDetailView : UIView
+
+- (instancetype)initWithPrice:(NSNumber *)price;
+
+@end
