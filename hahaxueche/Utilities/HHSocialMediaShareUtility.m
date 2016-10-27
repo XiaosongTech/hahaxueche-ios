7a0e0d5a353669c7b82979fabb872be2fc048c3b
Zixiao Wang <wangzixiao0330@gmail.com>
2016-10-27 14:32:44 +0800

    add deepshare & polish

--- hahaxueche/Utilities/HHSocialMediaShareUtility.m
+++ hahaxueche/Utilities/HHSocialMediaShareUtility.m
@@ -15,8 +15,6 @@
 #import "NSString+HHURL.h"
 #import "HHStudentStore.h"
 
-static NSString *const kStagingReferalBaseURL = @"http://staging-api.hahaxueche.net/share/invitations?target=%@&city_id=%@";
-static NSString *const kProdReferalBaseURL = @"http://api.hahaxueche.net/share/invitations?target=%@&city_id=%@";
 
 static NSString *const kStagingShareCoachBaseURL = @"http://staging-api.hahaxueche.net/share/coaches/%@?target=%@";
 static NSString *const kProdShareCoachBaseURL = @"http://api.hahaxueche.net/share/coaches/%@?target=%@";
@@ -255,98 +253,6 @@ static NSString *const kSupportQQ = @"3319762526";
 
 }
 
-- (void)generateUserReferLinkWithShareType:(ShareType)shareType completion:(MessageCompletion)completion {
-    [[HHLoadingViewUtility sharedInstance] showLoadingView];
-    OSMessage *msg = [[OSMessage alloc] init];
-    msg.image = [UIImage imageNamed:@"ic_share"];
-    msg.thumbnail = [UIImage imageNamed:@"ic_share"];
-    NSString *baseURL = nil;
-    
-#ifdef DEBUG
-    baseURL = kStagingReferalBaseURL;
-#else
-    baseURL = kProdReferalBaseURL;
-    
-#endif
-
-    
-    switch (shareType) {
-        case ShareTypeQQ: {
-            msg.multimediaType = OSMultimediaTypeNews;
-            msg.title = @"墙裂推荐:哈哈学车";
-            msg.desc = @"注册立享50元优惠";
-            [[HHSocialMediaShareUtility sharedInstance] generateBranchLinkForUserReferWithCompletion:^(NSString *url, NSError *error) {
-                [[HHLoadingViewUtility sharedInstance] dismissLoadingView];
-                msg.link = [NSString stringWithFormat:baseURL, [url urlEncode], [[HHStudentStore sharedInstance].currentStudent.cityId stringValue]];
-                if (completion) {
-                    completion(msg);
-                }
-            }];
-            
-        } break;
-            
-        case ShareTypeWeibo: {
-            [[HHSocialMediaShareUtility sharedInstance] generateBranchLinkForUserReferWithCompletion:^(NSString *url, NSError *error) {
-                [[HHLoadingViewUtility sharedInstance] dismissLoadingView];
-                msg.title = [NSString stringWithFormat:@"墙裂推荐:哈哈学车 注册立享50元优惠 %@", [NSString stringWithFormat:baseURL, [url urlEncode], [[HHStudentStore sharedInstance].currentStudent.cityId stringValue]]];
-                if (completion) {
-                    completion(msg);
-                }
-            }];
-        } break;
-            
-        case ShareTypeWeChat: {
-            msg.multimediaType = OSMultimediaTypeNews;
-            msg.title = @"墙裂推荐:哈哈学车";
-            msg.desc = @"注册立享50元优惠";
-            [[HHSocialMediaShareUtility sharedInstance] generateBranchLinkForUserReferWithCompletion:^(NSString *url, NSError *error) {
-                [[HHLoadingViewUtility sharedInstance] dismissLoadingView];
-                msg.link = [NSString stringWithFormat:baseURL, [url urlEncode], [[HHStudentStore sharedInstance].currentStudent.cityId stringValue]];
-                if (completion) {
-                    completion(msg);
-                }
-            }];
-        } break;
-            
-        case ShareTypeWeChatTimeLine: {
-            [[HHSocialMediaShareUtility sharedInstance] generateBranchLinkForUserReferWithCompletion:^(NSString *url, NSError *error) {
-                [[HHLoadingViewUtility sharedInstance] dismissLoadingView];
-                msg.title = [NSString stringWithFormat:@"墙裂推荐:哈哈学车 注册立享50元优惠"];
-                msg.link = [NSString stringWithFormat:baseURL, [url urlEncode], [[HHStudentStore sharedInstance].currentStudent.cityId stringValue]];
-                if (completion) {
-                    completion(msg);
-                }
-            }];
-        } break;
-            
-        default:
-            break;
-    }
-}
-
-- (void)generateBranchLinkForUserReferWithCompletion:(callbackWithUrl)completion {
-    
-    if ([HHSocialMediaShareUtility sharedInstance].userReferBranchLink) {
-        if (completion) {
-            completion([HHSocialMediaShareUtility sharedInstance].userReferBranchLink, nil);
-            return;
-        }
-    }
-    
-    BranchUniversalObject *branchUniversalObject = [[BranchUniversalObject alloc] initWithCanonicalIdentifier:[HHStudentStore sharedInstance].currentStudent.studentId];
-    branchUniversalObject.title = @"Share Refer Link";
-    branchUniversalObject.contentDescription = @"Share Refer Link";
-    [branchUniversalObject addMetadataKey:@"refererId" value:[HHStudentStore sharedInstance].currentStudent.userId];
-    
-    BranchLinkProperties *linkProperties = [[BranchLinkProperties alloc] init];
-    linkProperties.feature = @"user refer";
-    linkProperties.channel = @"iOS";
-    [branchUniversalObject getShortUrlWithLinkProperties:linkProperties andCallback:^(NSString *url, NSError *error) {
-        if (completion) {
-            completion(url, error);
-        }
-    }];
-}
 
 + (void)generateBranchLink:(HHCoach *)coach completion:(callbackWithUrl)completion {
     
@@ -365,59 +271,7 @@ static NSString *const kSupportQQ = @"3319762526";
     }];
 }
 
-- (void)shareUserLinkWithType:(ShareType)shareType {
-    switch (shareType) {
-        case ShareTypeQQ: {
-            if (![OpenShare isQQInstalled]) {
-                [[HHToastManager sharedManager] showErrorToastWithText:@"请先安装手机QQ应用, 然后重试"];
-                return;
-            }
-            
-            [[HHSocialMediaShareUtility sharedInstance] generateUserReferLinkWithShareType:shareType completion:^(OSMessage *message) {
-                [OpenShare shareToQQFriends:message Success:nil Fail:nil];
-            }];
-        } break;
-            
-        case ShareTypeWeibo: {
-            if (![OpenShare isWeiboInstalled]) {
-                [[HHToastManager sharedManager] showErrorToastWithText:@"请先安装手机微博应用, 然后重试"];
-                return;
-            }
-            [[HHSocialMediaShareUtility sharedInstance] generateUserReferLinkWithShareType:shareType completion:^(OSMessage *message) {
-                [OpenShare shareToWeibo:message Success:nil Fail:nil];
-            }];
 
-            
-        } break;
-            
-        case ShareTypeWeChat: {
-            if (![OpenShare isWeixinInstalled]) {
-                [[HHToastManager sharedManager] showErrorToastWithText:@"请先安装手机微信应用, 然后重试"];
-                return;
-            }
-            [[HHSocialMediaShareUtility sharedInstance] generateUserReferLinkWithShareType:shareType completion:^(OSMessage *message) {
-                [OpenShare shareToWeixinSession:message Success:nil Fail:nil];
-            }];
-            
-            
-        } break;
-            
-        case ShareTypeWeChatTimeLine: {
-            if (![OpenShare isWeixinInstalled]) {
-                [[HHToastManager sharedManager] showErrorToastWithText:@"请先安装手机微信应用, 然后重试"];
-                return;
-            }
-            
-            [[HHSocialMediaShareUtility sharedInstance] generateUserReferLinkWithShareType:shareType completion:^(OSMessage *message) {
-                [OpenShare shareToWeixinTimeline:message Success:nil Fail:nil];
-            }];
-            
-        } break;
-            
-        default:
-            break;
-    }
-}
 
 - (void)shareMyQRCode:(UIImage *)qrCode shareType:(ShareType)shareType {
     OSMessage *msg = [[OSMessage alloc] init];
@@ -479,24 +333,9 @@ static NSString *const kSupportQQ = @"3319762526";
     }
 }
 
-
-- (void)getUserReferLinkWithCompletion:(LinkCompletion)completion {
+- (void)sharePersonalCoach:(HHPersonalCoach *)coach shareType:(ShareType)shareType {
     
-    NSString *baseURL = nil;
-    
-#ifdef DEBUG
-    baseURL = kStagingReferalBaseURL;
-#else
-    baseURL = kProdReferalBaseURL;
-    
-#endif
-    
-    [[HHSocialMediaShareUtility sharedInstance] generateBranchLinkForUserReferWithCompletion:^(NSString *url, NSError *error) {
-        [[HHLoadingViewUtility sharedInstance] dismissLoadingView];
-        if (completion) {
-            completion([NSString stringWithFormat:baseURL, [url urlEncode], [[HHStudentStore sharedInstance].currentStudent.cityId stringValue]]);
-        }
-    }];
 }
+
 
 @end
